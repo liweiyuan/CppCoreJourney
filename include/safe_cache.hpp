ac888e3c1974f7161aea5ec6c4dbb2a1909dfea0
liwy <535767595@qq.com>
2025-02-08 14:55:58 +0800

    优化代码

--- include/safe_cache.hpp
+++ include/safe_cache.hpp
@@ -10,7 +10,7 @@
 // SafeLRUCache 是一个线程安全的LRU（最近最少使用）缓存实现。
 // 它使用了一个双向链表（std::list）来维护缓存项的访问顺序，
 // 并使用一个哈希表（std::unordered_map）来快速查找缓存项。
-// 通过互斥锁（std::mutex）来保证线程安全。
+// 通过锁（std::shared_mutex）来保证线程安全。
 template <typename Key, typename Value>
 class SafeLRUCache {
 public:
@@ -22,14 +22,25 @@ public:
     // @param key: 要查找的键
     // @return: 如果找到对应的值，则返回std::optional<Value>，否则返回std::nullopt
     std::optional<Value> get(const Key& key){
-        std::shared_lock<std::shared_mutex> lock(mutex_); // 加共享锁(读锁)，保证线程安全
+        //乐观锁
+        std::shared_lock<std::shared_mutex> read_ock(mutex_); // 加共享锁(读锁)，保证线程安全
         auto it = cache_map_.find(key); // 在哈希表中查找键
         if (it == cache_map_.end()) {
             return std::nullopt;// 如果未找到，返回空值
         }
-        // 将找到的缓存项移动到链表的最前面，表示最近使用
-        cache_list_.splice(cache_list_.begin(), cache_list_, it->second);
-        return it->second->second;// 返回找到的值
+
+        //找到对应的缓存项
+        Value value = it->second->second; // 获取值
+        //检查是否要更新链表
+        if (it->second != cache_list_.begin()) {
+            //释放读锁
+            read_ock.unlock();
+            //加写锁
+            std::lock_guard<std::shared_mutex> write_lock(mutex_); // 加独占锁(写锁)，保证线程安全
+            //将找到的缓存项移动到链表的最前面，表示最近使用
+            cache_list_.splice(cache_list_.begin(), cache_list_, it->second);
+        }
+        return value;// 返回找到的值
     }
 
     // 插入或更新缓存项
@@ -52,6 +63,30 @@ public:
         }
         // 将新的缓存项插入到链表的最前面，并在哈希表中记录其位置
         cache_list_.emplace_front(key, value);
+        cache_map_[key] = cache_list_.begin();
+    }
+
+    // 插入或者更新缓存项 使用移动语义的版本
+    // @param key: 要插入或更新的键
+    // @param value: 要插入或更新的值
+    // 使用移动语义
+    void put(const Key& key, Value&& value) {
+        std::lock_guard<std::shared_mutex> lock(mutex_);  // 加锁，保证线程安全
+        auto it = cache_map_.find(key);  // 在哈希表中查找键
+        if (it != cache_map_.end()) {
+            // 如果键已存在，更新其值并将其移动到链表的最前面
+            cache_list_.splice(cache_list_.begin(), cache_list_, it->second);
+            it->second->second = std::move(value);
+            return;
+        }
+        // 如果缓存已满，移除链表末尾的缓存项（最近最少使用的项）
+        if (cache_list_.size() == capacity_) {
+            auto last = cache_list_.back();
+            cache_map_.erase(last.first);  // 从哈希表中移除
+            cache_list_.pop_back();  // 从链表中移除
+        }
+        // 将新的缓存项插入到链表的最前面，并在哈希表中记录其位置
+        cache_list_.emplace_front(key, std::move(value));
         cache_map_[key] = cache_list_.begin();
     }
 
--- test/test_cache.cpp
+++ test/test_cache.cpp
@@ -4,13 +4,20 @@
 
 TEST(SafeLRUCacheTests, test_safe_lru_cache) {
     SafeLRUCache<int, std::string> cache(2);
-    cache.put(1, "one");
-    cache.put(2, "two");
-    EXPECT_EQ(cache.get(1).value(), "one");
+    cache.put(1, "one"); //移动语义
+    cache.put(2, "two"); //移动语义
+    EXPECT_EQ(cache.get(1).value(), "one"); //移动语义
     cache.put(3, "three");
     EXPECT_FALSE(cache.get(2).has_value());
     EXPECT_EQ(cache.get(1).value(), "one");
     EXPECT_EQ(cache.get(3).value(), "three");
+
+    std::string value("four"); //拷贝语义
+    cache.put(4, value);
+    EXPECT_TRUE(cache.get(3).has_value());
+    EXPECT_FALSE(cache.get(1).has_value());
+    EXPECT_EQ(cache.get(3).value(), "three");
+    EXPECT_EQ(cache.get(4).value(), "four");
 }
 
 int main(int argc, char **argv) {
