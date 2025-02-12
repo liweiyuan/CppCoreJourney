1a6c6bf25840b4a8c6252fb40aa9f63e5146d14e
liwy <535767595@qq.com>
2025-02-12 13:56:21 +0800

    使用RALL管理内存

--- include/skiplist.hpp
+++ include/skiplist.hpp
@@ -1,20 +1,24 @@
-#pragma
+#ifndef __SKIPLIST_HPP__
+#define __SKIPLIST_HPP__
+#pragma once
 #include <cstdlib>
 #include <ctime>
 #include <iostream>
 #include <limits> // Required for numeric_limits
 #include <vector>
+#include <memory>
 
+// 定义一个泛型类 SkipList，用于存储类型 T 的元素。
 template <typename T> class SkipList {
   private:
     struct Node {
         T key;
-        std::vector<Node *> next;
+        std::vector<std::shared_ptr<Node>> next; // 指向不同层次的下一个节点的指针。
 
         Node(const T &key, int level) : key(key), next(level, nullptr) {}
     };
 
-    Node *head;
+    std::shared_ptr<Node> head;
     int level;
     double p;
 
@@ -29,22 +33,13 @@ template <typename T> class SkipList {
   public:
     SkipList(int maxLevel = 16, double probability = 0.5)
         : level(maxLevel), p(probability) {
-        head = new Node(std::numeric_limits<T>::min(),
+        head = std::make_shared<Node>(std::numeric_limits<T>::min(),
                         level); // 使用最小可能值作为头节点键值
     }
 
-    ~SkipList() {
-        Node *current = head;
-        while (current != nullptr) {
-            Node *next = current->next[0];
-            delete current;
-            current = next;
-        }
-    }
-
     void insert(const T &key) {
-        std::vector<Node *> update(level);
-        Node *current = head;
+        std::vector<std::shared_ptr<Node>> update(level);
+        auto current = head;
 
         for (int i = level - 1; i >= 0; i--) {
             while (current->next[i] != nullptr && current->next[i]->key < key) {
@@ -62,7 +57,7 @@ template <typename T> class SkipList {
             level = l;
         }
 
-        Node *newNode = new Node(key, l);
+        auto newNode = std::make_shared<Node>(key, l);
 
         for (int i = 0; i < l; i++) {
             newNode->next[i] = update[i]->next[i];
@@ -71,7 +66,7 @@ template <typename T> class SkipList {
     }
 
     bool search(const T &key) const { // const correctness
-        Node *current = head;
+        auto current = head;
 
         for (int i = level - 1; i >= 0; i--) {
             while (current->next[i] != nullptr && current->next[i]->key < key) {
@@ -88,7 +83,7 @@ template <typename T> class SkipList {
     void print() const { // const correctness
         for (int i = level - 1; i >= 0; i--) {
             std::cout << "Level " << i << ": ";
-            Node *current = head->next[i];
+            auto current = head->next[i];
             while (current != nullptr) {
                 std::cout << current->key << " ";
                 current = current->next[i];
@@ -97,3 +92,5 @@ template <typename T> class SkipList {
         }
     }
 };
+
+#endif
