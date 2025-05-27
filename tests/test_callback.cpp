45a9ec77367da16cf1e909457d01b5fce9402d6f
liwy <535767595@qq.com>
2025-05-27 18:34:37 +0800

    补充测试

--- tests/test_callback.cpp
+++ tests/test_callback.cpp
@@ -3,31 +3,33 @@
 #include <vector>
 
 class CallbackTest : public ::testing::Test {
-  protected:
-    std::vector<std::function<void(int)>> callbacks;
+protected:
+  std::vector<std::function<void(int)>> callbacks;
 
-    void SetUp() override {
-        callbacks.push_back(
-            [](int x) { std::cout << "Callback 1: " << x << std::endl; });
-        callbacks.push_back(
-            [](int x) { std::cout << "Callback 2: " << x << std::endl; });
-    }
+  void SetUp() override {
+    callbacks.push_back(
+        [](int x) { std::cout << "Callback 1: " << x << std::endl; });
+    callbacks.push_back(
+        [](int x) { std::cout << "Callback 2: " << x << std::endl; });
+  }
+
+  void TearDown() override { callbacks.clear(); }
 };
 
 TEST_F(CallbackTest, AddCallback) { EXPECT_EQ(callbacks.size(), 2); }
 
 TEST_F(CallbackTest, ExecuteCallbacks) {
-    for (auto &callback : callbacks) {
-        callback(42);
-    }
+  for (auto &callback : callbacks) {
+    callback(42);
+  }
 }
 
 TEST_F(CallbackTest, ClearCallbacks) {
-    callbacks.clear();
-    EXPECT_TRUE(callbacks.empty());
+  callbacks.clear();
+  EXPECT_TRUE(callbacks.empty());
 }
 
 int main(int argc, char **argv) {
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
